01081debd7f9d0f246643fabbd6c5446eabc5f8c
Fabio Brzostek Muller <fabiobrzm@gmail.com>
2020-03-22 18:32:27 -0300

    Add adding functions

--- list_manager.py
+++ list_manager.py
@@ -1,9 +1,12 @@
 import argparse
 import logging
+import readline
 import sys
 
 
-logging.basicConfig(format="%(asctime)s - [%(levelname)s] - %(message)s")
+logging.basicConfig(
+    format="%(asctime)s - [%(levelname)s] - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger()
 
 
@@ -54,15 +57,81 @@ def load_from_file(file):
         with open(file, encoding="utf-8") as f:
             return f.read().splitlines()
     except FileNotFoundError:
-        logger.critical("File '%s' could not found, exiting.", file)
+        logger.critical("File '%s' could not found, exiting", file)
         sys.exit(1)
     except PermissionError:
-        logger.critical("No permission to read file '%s', exiting.", file)
+        logger.critical("No permission to read file '%s', exiting", file)
         sys.exit(1)
 
 
 def save_to_file(file_dir_list, file):
-    pass
+    """Write the files/directories list to the supplied file."""
+    try:
+        with open(file, "w", encoding="utf-8") as f:
+            f.write(display_list(file_dir_list))
+    except PermissionError:
+        logger.critical("No permission to open or write to file '%s', exiting", file)
+        sys.exit(1)
+
+
+def get_input(multiple=False):
+    """Get file/directory input from stdin."""
+    readline.parse_and_bind("")
+
+    lines = []
+    print("Enter the " + ("files/directories" if multiple else "file/directory"))
+
+    while True:
+        try:
+            lines.append(input())
+            if not multiple:
+                break
+        except EOFError:
+            break
+
+    return lines
+
+
+def add_to_list(file_dir_list, entries):
+    """Add entries to the files/directories list."""
+    modified = False
+    for entry in entries:
+        if entry.find("//") != -1:
+            logger.info("New entry '%s' is not valid and will not be added", entry)
+        elif entry in file_dir_list:
+            logger.info("New entry '%s' is a duplicate and will not be added", entry)
+        else:
+            existing_children = [
+                item
+                for item in file_dir_list
+                if item.startswith(entry + ("/" if entry[-1] != "/" else ""))
+            ]
+            for child in existing_children:
+                logger.info(
+                    "Removing entry '%s' as it is contained in new entry '%s'",
+                    child,
+                    entry,
+                )
+                file_dir_list.remove(child)
+
+            possible_parents = [
+                item
+                for item in file_dir_list
+                if entry.rstrip("/").startswith(item + "/")
+            ]
+            if len(possible_parents) > 0:
+                logger.info(
+                    "New entry '%s' will not be added since it is already contained in entry '%s'",
+                    entry,
+                    possible_parents[0],
+                )
+                continue
+
+            file_dir_list.append(entry.rstrip("/"))
+            modified = True
+
+    if modified:
+        file_dir_list.sort()
 
 
 def display_list(file_dir_list, concatenate=False):
@@ -79,9 +148,11 @@ def main():
     file_dir_list = load_from_file(args.file)
 
     if args.add:
-        pass
+        add_to_list(file_dir_list, get_input())
+        save_to_file(file_dir_list, args.file)
     elif args.add_multiple:
-        pass
+        add_to_list(file_dir_list, get_input(multiple=True))
+        save_to_file(file_dir_list, args.file)
     elif args.remove:
         pass
     elif args.remove_multiple:
--- tests.py
+++ tests.py
@@ -1,10 +1,67 @@
+import logging
 from unittest import TestCase
 
-from list_manager import display_list
+from list_manager import add_to_list, display_list
+
+
+example_list = [
+    "~/test/file",
+    "~/test/subdir/file",
+    "~/test/subdir/other_file",
+    "~/test/subdir/subdir/file",
+    "~/test/subdir/subdir/other_other_file",
+    "~/test/subdir2",
+]
 
 
 class AddTestCase(TestCase):
-    pass
+    def setUp(self):
+        logging.disable(logging.CRITICAL)
+        self.initial_list = [*example_list]
+
+    def test_add_no_entries(self):
+        add_to_list(self.initial_list, [])
+        self.assertEqual(self.initial_list, example_list)
+
+    def test_add_invalid(self):
+        add_to_list(self.initial_list, ["~/test//file/x"])
+        self.assertEqual(self.initial_list, example_list)
+
+    def test_add_duplicates(self):
+        add_to_list(self.initial_list, ["~/test/file", "~/test/subdir/file"])
+        self.assertEqual(self.initial_list, example_list)
+
+    def test_add_already_contained(self):
+        add_to_list(
+            self.initial_list, ["~/test/subdir2/file", "~/test/subdir2/other_subdir2"]
+        )
+        self.assertEqual(self.initial_list, example_list)
+
+    def test_add_parent(self):
+        add_to_list(self.initial_list, ["~/test/subdir/"])
+        self.assertEqual(
+            self.initial_list, ["~/test/file", "~/test/subdir", "~/test/subdir2"]
+        )
+
+    def test_add_simple_entries(self):
+        add_to_list(
+            self.initial_list,
+            ["~/test/file2", "~/test/subdir/file3", "~/test/subdir/subdir/file4"],
+        )
+        self.assertEqual(
+            self.initial_list,
+            [
+                "~/test/file",
+                "~/test/file2",
+                "~/test/subdir/file",
+                "~/test/subdir/file3",
+                "~/test/subdir/other_file",
+                "~/test/subdir/subdir/file",
+                "~/test/subdir/subdir/file4",
+                "~/test/subdir/subdir/other_other_file",
+                "~/test/subdir2",
+            ],
+        )
 
 
 class RemoveTestCase(TestCase):
@@ -13,13 +70,7 @@ class RemoveTestCase(TestCase):
 
 class DisplayTestCase(TestCase):
     def setUp(self):
-        self.regular_list = [
-            "~/test/file",
-            "~/test/dir/file",
-            "~/test/dir/other_file",
-            "~/test/dir/subdir/file",
-            "~/test/dir/subdir/other_other_file",
-        ]
+        self.regular_list = [*example_list]
 
     def test_display_empty_list(self):
         self.assertEqual(display_list([]), "")
@@ -28,11 +79,11 @@ class DisplayTestCase(TestCase):
     def test_regular_list(self):
         self.assertEqual(
             display_list(self.regular_list),
-            "~/test/file\n~/test/dir/file\n~/test/dir/other_file\n~/test/dir/subdir/file\n~/test/dir/subdir/other_other_file",
+            "~/test/file\n~/test/subdir/file\n~/test/subdir/other_file\n~/test/subdir/subdir/file\n~/test/subdir/subdir/other_other_file\n~/test/subdir2",
         )
 
     def test_regular_list_concatenated(self):
         self.assertEqual(
             display_list(self.regular_list, concatenate=True),
-            "~/test/file ~/test/dir/file ~/test/dir/other_file ~/test/dir/subdir/file ~/test/dir/subdir/other_other_file",
+            "~/test/file ~/test/subdir/file ~/test/subdir/other_file ~/test/subdir/subdir/file ~/test/subdir/subdir/other_other_file ~/test/subdir2",
         )

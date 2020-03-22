50d30e5820ddddc489df1827f072b970b8003640
Fabio Brzostek Muller <fabiobrzm@gmail.com>
2020-03-22 16:52:44 -0300

    Add display functions and part of the main structure

--- list_manager.py
+++ list_manager.py
@@ -1,38 +1,96 @@
 import argparse
+import logging
+import sys
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "file", help="the path to the file where the file list will be stored"
-)
+logging.basicConfig(format="%(asctime)s - [%(levelname)s] - %(message)s")
+logger = logging.getLogger()
 
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument(
-    "-a", "--add", help="add a single file/directory path", action="store_true"
-)
-group.add_argument(
-    "-A",
-    "--add-multiple",
-    help="add multiple, new-line separated, file/directory paths",
-    action="store_true",
-)
-group.add_argument(
-    "-r", "--remove", help="remove a single file/directory path", action="store_true"
-)
-group.add_argument(
-    "-R",
-    "--remove-multiple",
-    help="remove multiple, new-line separated, file/directory paths",
-    action="store_true",
-)
-group.add_argument(
-    "-d", "--display", help="display the current list in the file", action="store_true"
-)
-group.add_argument(
-    "-c",
-    "--concatenate",
-    help="display the current list in the file with the lines concatenated and joined by a space",
-    action="store_true",
-)
 
-args = parser.parse_args()
+def setup_args(parser):
+    parser.add_argument(
+        "file", help="the path to the file where the file list will be stored"
+    )
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "-a", "--add", help="add a single file/directory path", action="store_true"
+    )
+    group.add_argument(
+        "-A",
+        "--add-multiple",
+        help="add multiple, new-line separated, file/directory paths",
+        action="store_true",
+    )
+    group.add_argument(
+        "-r",
+        "--remove",
+        help="remove a single file/directory path",
+        action="store_true",
+    )
+    group.add_argument(
+        "-R",
+        "--remove-multiple",
+        help="remove multiple, new-line separated, file/directory paths",
+        action="store_true",
+    )
+    group.add_argument(
+        "-d",
+        "--display",
+        help="display the current list in the file",
+        action="store_true",
+    )
+    group.add_argument(
+        "-c",
+        "--concatenate",
+        help="display the current list in the file with the lines concatenated and joined by a space",
+        action="store_true",
+    )
+
+
+def load_from_file(file):
+    """Load the files/directories list from the supplied file."""
+    try:
+        with open(file, encoding="utf-8") as f:
+            return f.read().splitlines()
+    except FileNotFoundError:
+        logger.critical("File '%s' could not found, exiting.", file)
+        sys.exit(1)
+    except PermissionError:
+        logger.critical("No permission to read file '%s', exiting.", file)
+        sys.exit(1)
+
+
+def save_to_file(file_dir_list, file):
+    pass
+
+
+def display_list(file_dir_list, concatenate=False):
+    """Return the files/directories list to be displayed in stdout."""
+    return (" " if concatenate else "\n").join(file_dir_list)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    setup_args(parser)
+
+    args = parser.parse_args()
+
+    file_dir_list = load_from_file(args.file)
+
+    if args.add:
+        pass
+    elif args.add_multiple:
+        pass
+    elif args.remove:
+        pass
+    elif args.remove_multiple:
+        pass
+    elif args.display:
+        print(display_list(file_dir_list))
+    elif args.concatenate:
+        print(display_list(file_dir_list, concatenate=True))
+
+
+if __name__ == "__main__":
+    main()
--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from list_manager import display_list
+
 
 class AddTestCase(TestCase):
     pass
@@ -10,4 +12,27 @@ class RemoveTestCase(TestCase):
 
 
 class DisplayTestCase(TestCase):
-    pass
+    def setUp(self):
+        self.regular_list = [
+            "~/test/file",
+            "~/test/dir/file",
+            "~/test/dir/other_file",
+            "~/test/dir/subdir/file",
+            "~/test/dir/subdir/other_other_file",
+        ]
+
+    def test_display_empty_list(self):
+        self.assertEqual(display_list([]), "")
+        self.assertEqual(display_list([], concatenate=True), "")
+
+    def test_regular_list(self):
+        self.assertEqual(
+            display_list(self.regular_list),
+            "~/test/file\n~/test/dir/file\n~/test/dir/other_file\n~/test/dir/subdir/file\n~/test/dir/subdir/other_other_file",
+        )
+
+    def test_regular_list_concatenated(self):
+        self.assertEqual(
+            display_list(self.regular_list, concatenate=True),
+            "~/test/file ~/test/dir/file ~/test/dir/other_file ~/test/dir/subdir/file ~/test/dir/subdir/other_other_file",
+        )
